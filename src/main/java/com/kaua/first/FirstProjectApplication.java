45344631c8a7b25981ad8d574deb63289a111530
Kauã Pereira <zkauacodes@hotmail.com>
2023-01-21 14:38:46 -0300

    feat: add Logging to start application with SLF4J lib

--- src/main/java/com/kaua/first/FirstProjectApplication.java
+++ src/main/java/com/kaua/first/FirstProjectApplication.java
@@ -1,12 +1,18 @@
 package com.kaua.first;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class FirstProjectApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(FirstProjectApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Server starting...");
 		SpringApplication.run(FirstProjectApplication.class, args);
+		logger.info("Server running");
 	}
 }
